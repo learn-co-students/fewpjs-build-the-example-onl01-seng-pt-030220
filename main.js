8393b7bc0770c28db6e0ec1e8f089aa39f1b5129
SincerelyBrittany <sincerelybr@gmail.com>
2020-09-03 16:12:26 -0400

    adds event listener to document and like

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
+
+
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+
+document.addEventListener('click', (e) =>{
+  let like = document.querySelector('.like-glyph')
+  console.log(like)
+console.log(e)
+})
 
 
 
