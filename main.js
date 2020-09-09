d6c03349ca179c8b95abb1d0fce28af9c411c0cf
Rkebede <rutht.kebede@gmail.com>
2020-09-09 18:46:44 -0500

    Done.

--- main.js
+++ main.js
@@ -3,6 +3,61 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.getElementById('modal')
+modal.className = 'hidden'
+
+function hide(){
+  modal.setAttribute('class','hidden')
+}
+
+document.addEventListener("DOMContentLoaded", () =>{
+  document.addEventListener("click", likedPost)
+})
+
+function likedPost(e){
+  if (e.target.classList.contains("like-glyph")){
+    mimicServerCall()
+      .then(() => heartToggle(e.target))
+      .catch((error) => failure(error))
+  }
+}
+
+function heartToggle(target){
+  if (target.innerText === EMPTY_HEART){
+    target.innerText = FULL_HEART
+    target.classList.toggle(("activated-heart"))
+  }
+  else {
+    target.innerText = EMPTY_HEART
+    target.classList.toggle(("activated-heart"))
+  }
+}
+
+function failure(error){
+  modal.classList.remove("hidden")
+  modal.innerText = error
+  console.log(error)
+  setTimeout(hide, 5000)
+}
+
+function hide(){
+  modal.setAttribute('class','hidden')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
